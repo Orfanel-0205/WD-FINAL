57554928f104cab820043d332e3bbc1c80ae97f2
Orfanel-0205 <202211750@fit.edu.ph>
2024-07-15 03:32:35 +0800

    Refactor: renew code structure

--- pages/Projects/assets/js/script.js
+++ pages/Projects/assets/js/script.js
@@ -1,13 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const mainImg = document.getElementById('main-img');
-
-    mainImg.addEventListener('mouseover', function() {
-        setTimeout(function() {
-            mainImg.src = 'assets/img/450590030_393644869917791_4972001812372923820_n.jpg';
-        }, 4); 
-    });
-
-    mainImg.addEventListener('mouseout', function() {
-        mainImg.src = 'assets/img/441930533_975816594262388_8791712594973653961_n.jpg';
+document.addEventListener('DOMContentLoaded', function () {
+    const photoFrames = document.querySelectorAll('.photo-frame');
+    
+    photoFrames.forEach(frame => {
+        frame.addEventListener('click', function () {
+            this.classList.toggle('zoom');
+        });
     });
 });
